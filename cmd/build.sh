f94823abc287c02a61e048722647a29305e98afa
weiqiang <weiqiang@doumeng>
2022-10-25 16:07:06 +0800

    update README, 完善 grafana json, update build shell

--- cmd/build.sh
+++ cmd/build.sh
@@ -5,7 +5,14 @@ export GOARCH=amd64
 export GOOS=linux
 export GCCGO=gc
 
-go build -o bandwagonhost_cloud_exporter bandwagonhost_cloud_exporter.go
+version=$1
 
-tar -zcvf bandwagonhost_cloud_exporter-linux-amd64.tar.gz \
+if [ -z $version ]; then
+    version=v0.1
+fi
+
+go build -o bandwagonhost_cloud_exporter bandwagonhost_cloud_exporter.go
+chmod +x bandwagonhost_cloud_exporter
+
+tar -zcvf bandwagonhost_cloud_exporter-linux-amd64-${version}.tar.gz \
   bandwagonhost_cloud_exporter config/bandwagonhost_cloud_exporter.yaml config/bandwagonhost_cloud_exporter.service README.md
